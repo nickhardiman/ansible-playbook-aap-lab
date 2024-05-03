e2c97b589fd7c592ff1084d8f8f0cb1c022c32ac
Nick Hardiman <nick@email-domain.com>
2024-05-03 19:09:27 +0100

    start bash bootstrap scripts

---
+++ bootstrap-refarch-1.sh
@@ -0,0 +1,35 @@
+#-------------------------
+# Set up the hypervisor 
+# Instructions 
+
+#-------------------------
+# Edit and change my details to yours.
+
+SITE1_IP=192.168.1.253
+SITE2_IP=192.168.1.162
+SITE3_IP=192.168.1.252
+USER=nick
+#-------------------------
+# do stuff
+
+add_machines_to_hosts () {
+     sudo cat << EOF >>  /etc/hosts
+# 3 machines make up the AAP home lab
+$SITE1_IP  host.site1.example.com         hostsite1
+$SITE2_IP  host.site2.example.com         hostsite2
+$SITE3_IP  host.site3.example.com         hostsite3
+EOF
+     # Encrypt the new file. 
+     echo 'my vault password' >  ~/my-vault-pass
+     ansible-vault encrypt --vault-pass-file ~/my-vault-pass ~/vault-credentials.yml
+}
+
+add_machines_to_hosts
+
+mkdir aap-refarch
+cd aap-refarch
+# Download this script.
+curl -O https://raw.githubusercontent.com/nickhardiman/ansible-playbook-refarch/main/bootstrap-site1.sh 
+scp bootstrap-site1.sh $USER@host.site1.example.com:
+ssh $USER@host.site1.example.com bootstrap-site1.sh
+
